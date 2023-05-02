7baa965404917924b8d94a3ff1d09981bbe47e84
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-05-02 19:57:50 +1000

    Use https for URLs

--- lib/scraperd/activity.rb
+++ lib/scraperd/activity.rb
@@ -1,26 +1,25 @@
-require 'simple-rss'
-require 'ostruct'
-require 'pry'
+require "simple-rss"
+require "ostruct"
+require "pry"
 
 module Scraperd
   class Activity < ::OpenStruct
-    
     def initialize(item)
       super(Hashie::Mash.new({
-          id:         item[:guid].force_encoding("UTF-8"),
-          title:      title_from_title(item[:title]),
-          year:       year_from_title(item[:title]),
-          score:      score_from_title(item[:title]),
-          link:       item[:link].force_encoding("UTF-8"),
-          film_link:  "http://letterboxd.com/film/#{nicetitle_from_url(item[:link])}/".force_encoding("UTF-8"),
-          watched_at: watched_at_from_description(item[:description]),
-          added_at:   item[:pubDate]
-        })
-      )      
+        id: item[:guid].force_encoding("UTF-8"),
+        title: title_from_title(item[:title]),
+        year: year_from_title(item[:title]),
+        score: score_from_title(item[:title]),
+        link: item[:link].force_encoding("UTF-8"),
+        film_link: "https://letterboxd.com/film/#{nicetitle_from_url(item[:link])}/".force_encoding("UTF-8"),
+        watched_at: watched_at_from_description(item[:description]),
+        added_at: item[:pubDate]
+      })
+      )
     end
 
     def nicetitle_from_url(url)
-      url.split('/').last
+      url.split("/").last
     end
 
     def watched_at_from_description(description)
@@ -29,24 +28,23 @@ module Scraperd
     end
 
     def title_from_title(title)
-      title.split(' - ')[0..-2].join(' - ')
-        .split(',')[0..-2].join(',')
+      title.split(" - ")[0..-2].join(" - ")
+        .split(",")[0..-2].join(",")
         .strip
         .force_encoding("UTF-8")
     end
 
     def year_from_title(title)
-      title.split(' - ')[0..-2].join(' - ')
-        .split(',').last
+      title.split(" - ")[0..-2].join(" - ")
+        .split(",").last
         .strip
         .force_encoding("UTF-8")
     end
 
     def score_from_title(title)
-      html_score = title.split(' - ').last
+      html_score = title.split(" - ").last
       html_score.force_encoding("UTF-8")
-      html_score.split(//).map{|ind_score| ind_score == "★" ? 2 : 1 }.inject{|sum,x| sum + x }
+      html_score.split("").map { |ind_score| ind_score == "★" ? 2 : 1 }.inject { |sum, x| sum + x }
     end
-
   end
 end
--- scraperd.gemspec
+++ scraperd.gemspec
@@ -1,29 +1,28 @@
-# coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
+lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'scraperd/version'
+require "scraperd/version"
 
 Gem::Specification.new do |spec|
-  spec.name          = "scraperd"
-  spec.version       = Scraperd::VERSION
-  spec.authors       = ["Víctor Martín"]
-  spec.email         = ["victor.martin84@gmail.com"]
-  spec.summary       = %q{Scraper for Letterboxd user's activity}
-  spec.description   = %q{Scraper for Letterboxd user's activity using public user RSS}
-  spec.homepage      = "https://github.com/eltercero/scraperd"
-  spec.license       = "MIT"
+  spec.name = "scraperd"
+  spec.version = Scraperd::VERSION
+  spec.authors = ["Víctor Martín"]
+  spec.email = ["victor.martin84@gmail.com"]
+  spec.summary = "Scraper for Letterboxd user's activity"
+  spec.description = "Scraper for Letterboxd user's activity using public user RSS"
+  spec.homepage = "https://github.com/eltercero/scraperd"
+  spec.license = "MIT"
 
-  spec.files      = `git ls-files -z`.split("\x0")
+  spec.files = `git ls-files -z`.split("\x0")
   spec.test_files = `git ls-files -- {test,spec,features}/*`.split("\n")
 
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.executables = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "rake", '~> 10.1'
+  spec.add_development_dependency "bundler", "~> 2.0"
+  spec.add_development_dependency "rake", "~> 10.1"
 
-  spec.add_development_dependency 'minitest', '~>5.2'
-  spec.add_dependency 'simple-rss','~> 1.3', '>= 1.3.1'
-  spec.add_dependency 'hashie', '~>2.0'
+  spec.add_development_dependency "minitest", "~>5.2"
+  spec.add_dependency "simple-rss", "~> 1.3", ">= 1.3.1"
+  spec.add_dependency "hashie", "~>2.0"
 end
